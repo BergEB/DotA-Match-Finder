75629fab8e62401cf0e15095b7363be549eaecfe
BergEB <43139703+BergEB@users.noreply.github.com>
2023-04-13 17:59:26 -0500

    api test

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,12 +1,10 @@
-async function logJSONData() {
-  const response = await fetch("http://api.steampowered.com/IDOTA2Match_570/GetMatchHistory/v1?key=2827282DAF3A52E64919024532E0EBE1?account_id=76561198115356886");
-  const jsonData = await response.json();
-  console.log(jsonData);
-}
+fetch('http://api.steampowered.com/IDOTA2Match_570/GetMatchHistory/v1?key=2827282DAF3A52E64919024532E0EBE1?account_id=76561198115356886')
+  .then(function (response) {
+    console.log(response.json());
+  })
 
-async function logJSONData0() {
-  const response0 = await fetch("http://api.steampowered.com/IDOTA2Match_570/GetMatchHistory/v1?key=2827282DAF3A52E64919024532E0EBE1?account_id=76561198115356886");
-  const jsonData0 = await response0.json();
-  console.log(jsonData0);
-}
-    
+fetch('https://api.steampowered.com/ISteamUser/GetPlayerSummaries/v2/?key=2827282DAF3A52E64919024532E0EBE1&steamids=76561198115356886')
+  .then(function (response) {
+    console.log(response.json());
+  })
+
