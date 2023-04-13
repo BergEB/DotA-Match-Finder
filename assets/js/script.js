f2d3e9381159da7d1182ddac82b087a645b5eb75
BergEB <43139703+BergEB@users.noreply.github.com>
2023-04-13 18:20:29 -0500

    api test

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,20 +1,12 @@
-// Add Access Control Allow Origin headers
-app.use((req, res, next) => {
-  res.setHeader("Access-Control-Allow-Origin", "https://api.steampowered.com");
-  res.header(
-    "Access-Control-Allow-Headers",
-    "Origin, X-Requested-With, Content-Type, Accept"
-  );
-  next();
-});
-
 fetch('http://api.steampowered.com/IDOTA2Match_570/GetMatchHistory/v1/?key=2827282DAF3A52E64919024532E0EBE1?account_id=76561198115356886')
   .then(function (response) {
+    response.setHeader("Access-Control-Allow-Origin", "https://api.steampowered.com")
     console.log(response.json());
   })
 
 fetch('https://api.steampowered.com/ISteamUser/GetPlayerSummaries/v2/?key=2827282DAF3A52E64919024532E0EBE1&steamids=76561198115356886')
   .then(function (response) {
+    response.setHeader("Access-Control-Allow-Origin", "https://api.steampowered.com")
     console.log(response.json());
   })
 
