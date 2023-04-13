c50714887dcaeb23bf671ed5ca4f378043bf8d3c
BergEB <43139703+BergEB@users.noreply.github.com>
2023-04-13 18:37:18 -0500

    api test

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,12 +1,12 @@
 fetch('http://api.steampowered.com/IDOTA2Match_570/GetMatchHistory/v1/?key=2827282DAF3A52E64919024532E0EBE1?account_id=76561198115356886')
   .then(function (response) {
-    response.setHeader("Access-Control-Allow-Origin", "*")
+    res.setHeader("Access-Control-Allow-Origin", "*")
     console.log(response.json());
   })
 
 fetch('https://api.steampowered.com/ISteamUser/GetPlayerSummaries/v2/?key=2827282DAF3A52E64919024532E0EBE1&steamids=76561198115356886')
   .then(function (response) {
-    response.setHeader("Access-Control-Allow-Origin", "*")
+    res.setHeader("Access-Control-Allow-Origin", "*")
     console.log(response.json());
   })
 
