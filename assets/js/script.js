5a8192535f5c1f711ff0cbeaccf03a9ec1facc34
BergEB <43139703+BergEB@users.noreply.github.com>
2023-04-13 17:29:38 -0500

    api test

--- assets/js/script.js
+++ assets/js/script.js
@@ -2,3 +2,10 @@ fetch('http://api.steampowered.com/IDOTA2Match_570/GetMatchHistory/v1?key=A7E8E7
 	.then(function (response) {
       return response.json();
     })
+
+async function logJSONData() {
+  const response = await fetch("http://api.steampowered.com/IDOTA2Match_570/GetMatchHistory/v1?key=A7E8E7FCD7E40881E79BB9AD53ADE01A?account_id=76561198115356886");
+  const jsonData = await response.json();
+  console.log(jsonData);
+}
+    
