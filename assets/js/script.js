c771bd27d9714e86e2f086ef44e7ea8fdf5ee038
Penguuu <pengvang95@yahoo.com>
2023-04-14 08:47:55 -0500

    testing steamID fetch

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,12 +1,33 @@
-fetch('http://api.steampowered.com/IDOTA2Match_570/GetMatchHistory/v1/?key=2827282DAF3A52E64919024532E0EBE1?account_id=76561198115356886')
-  .then(function (response) {
-    res.setHeader("Access-Control-Allow-Origin", "*")
-    console.log(response.json());
-  })
 
-fetch('https://api.steampowered.com/ISteamUser/GetPlayerSummaries/v2/?key=2827282DAF3A52E64919024532E0EBE1&steamids=76561198115356886')
-  .then(function (response) {
-    res.setHeader("Access-Control-Allow-Origin", "*")
-    console.log(response.json());
-  })
 
+
+
+
+const buttonSubmit = document.getElementById('userSearch');
+var steamIDApi = 'https://api.steampowered.com/ISteamUser/GetPlayerSummaries/v2/?key=2827282DAF3A52E64919024532E0EBE1&steamids=';
+
+buttonSubmit.addEventListener("click", function (event) {
+  event.preventDefault();
+
+  const steamUserID = document.getElementById('userID').value;
+
+  fetch(steamIDApi + steamUserID)
+    .then(function (response) {
+      return response.json();
+    })
+    .then(function (data) {
+      console.log(data);
+document.getElementById('hi').innerText = data.response.players[0].personaname;
+// display_image(data.response.players[0].avatarfull);
+    });
+  //  document.getElementById('searchForm').reset();
+})
+
+
+/*
+function display_image(img) {
+  document.querySelector(".fact").src = img;
+  document.querySelector(".fact").style.visibility = "visible";
+ } */
+ 
+ 
