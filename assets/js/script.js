62abbf9c6ab09606d87270bb31d8a6bffc2b238e
Penguuu <pengvang95@yahoo.com>
2023-04-17 20:16:40 -0500

    more edits

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,10 +1,11 @@
-
-
-
-
-
 const buttonSubmit = document.getElementById('userSearch');
+const infoForm = document.getElementById("infoForm");
 var steamIDApi = 'https://api.steampowered.com/ISteamUser/GetPlayerSummaries/v2/?key=2827282DAF3A52E64919024532E0EBE1&steamids=';
+
+
+//hides the info page
+infoForm.classList.add("hidden");
+
 
 buttonSubmit.addEventListener("click", function (event) {
   event.preventDefault();
@@ -17,12 +18,20 @@ buttonSubmit.addEventListener("click", function (event) {
     })
     .then(function (data) {
       console.log(data);
-document.getElementById('hi').innerText = data.response.players[0].personaname;
+
+      // removes info page
+      infoForm.classList.remove("hidden");
+
+
+document.getElementById('steamID').innerText = data.response.players[0].steamid;
+document.getElementById('gameName').innerText = data.response.players[0].personaname;
+document.getElementById('profURL').innerText = data.response.players[0].profileurl;
+
+
 // display_image(data.response.players[0].avatarfull);
     });
   //  document.getElementById('searchForm').reset();
 })
-
 
 /*
 function display_image(img) {
@@ -30,4 +39,6 @@ function display_image(img) {
   document.querySelector(".fact").style.visibility = "visible";
  } */
  
- 
+
+
+var accountID = 'http://api.steampowered.com/IDOTA2Match_570/GetMatchHistory/v1/?key=2827282DAF3A52E64919024532E0EBE1&account_id=';
