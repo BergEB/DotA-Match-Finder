223f3ab6b09d60a8206723381cf97ddff456531c
Penguuu <pengvang95@yahoo.com>
2023-04-20 20:28:55 -0500

    adding first 5 game on user's steam list ( backup edit )

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,7 +1,10 @@
 const buttonSubmit = document.getElementById('userSearch');
 const infoForm = document.getElementById("infoForm");
 var steamIDApi = 'https://api.steampowered.com/ISteamUser/GetPlayerSummaries/v2/?key=2827282DAF3A52E64919024532E0EBE1&steamids=';
+var gameID = 'https://api.steampowered.com/IPlayerService/GetOwnedGames/v0001/?key=E73AF6D8FF004CC1716E3F08C637D90C&steamid=&include_appinfo=1';
 
+const apiGameKey = 'E73AF6D8FF004CC1716E3F08C637D90C';
+const gameList = document.getElementById('game-list');
 
 //hides the info page
 infoForm.classList.add("hidden");
@@ -12,33 +15,45 @@ buttonSubmit.addEventListener("click", function (event) {
 
   const steamUserID = document.getElementById('userID').value;
 
-  fetch(steamIDApi + steamUserID)
+  fetch(steamIDApi + steamUserID + gameID)
     .then(function (response) {
       return response.json();
     })
     .then(function (data) {
       console.log(data);
 
-      // removes info page
+      // removes info page until results are in
       infoForm.classList.remove("hidden");
 
-// info page
-document.getElementById('steamID').innerText = data.response.players[0].steamid;
-document.getElementById('gameName').innerText = data.response.players[0].personaname;
-document.getElementById('profURL').innerText = data.response.players[0].profileurl;
+      // info page
+      document.getElementById('steamID').innerText = data.response.players[0].steamid;
+      document.getElementById('gameName').innerText = data.response.players[0].personaname;
+      document.getElementById('profURL').innerText = data.response.players[0].profileurl;
 
-// displays Steam avatar
-display_image(data.response.players[0].avatarfull);
+      // displays Steam avatar
+      display_image(data.response.players[0].avatarfull);
     });
-document.getElementById('userSearch').reset();
-})
+});
 
 function display_image(img) {
   document.querySelector(".icon").src = img;
-document.querySelector("#Avatar").style.visibility = "visible";
- };
+};
 
- 
-
-/*
-var accountID = 'http://api.steampowered.com/IDOTA2Match_570/GetMatchHistory/v1/?key=2827282DAF3A52E64919024532E0EBE1&account_id='; */
+// displays game list
+const searchForm = document.getElementById('searchForm');
+searchForm.addEventListener('submit', event => {
+  event.preventDefault();
+  fetch(`https://api.steampowered.com/IPlayerService/GetOwnedGames/v0001/?key=E73AF6D8FF004CC1716E3F08C637D90C&steamid=&include_appinfo=1`)
+    .then(response => response.json())
+    .then(data => {
+      gameList.innerHTML = '';
+      for (let i = 0; i < 5 && i < data.response.games.length; i++) {
+        const game = data.response.games[i];
+        const gameName = game.name;
+        const listItem = document.createElement('li');
+        listItem.textContent = gameName;
+        gameList.appendChild(listItem);
+      }
+    })
+    .catch(error => console.log(error));
+});
